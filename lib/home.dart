088544de1e93ce694dcfcd4a8be81516082a09e0
Dwisaputr <121742036+Dwisaputr@users.noreply.github.com>
2024-05-01 19:48:34 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -13,86 +13,128 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+
+// Replace this entire method
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.titleLarge,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return Scaffold(
+    return  Scaffold(
       // TODO: Add app bar (102)
-      body: 
-      GridView.count(
-  crossAxisCount: 2,
-  padding: const EdgeInsets.all(16.0),
-  childAspectRatio: 8.0 / 9.0,
-  // TODO: Build a grid of cards (102)
-  children: <Widget>[
-    Card(
-      clipBehavior: Clip.antiAlias,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 18.0 / 11.0,
-          child: Image.asset('assets/diamond.png'),
+      appBar: AppBar(
+        title: const Text(
+          "SHRINE",
+          style: TextStyle(color: Colors.white),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Title'),
-              const SizedBox(height: 8.0),
-              Text('Secondary Text'),
-            ],
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+              semanticLabel: "Menu Button",
+            ),
+              onPressed: () {
+              print("menu clicked");
+         },
+        ),
+        // TODO: Add trailing buttons (102)
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
           ),
-        ),
-      ],
-    ),
-  )
-    ],
-),
-  appBar: AppBar(
-    title: const Text("ini judul",
-     style: TextStyle(color: Colors.white),
-     ),
-     backgroundColor: Colors.blue,
-     leading: IconButton(
-      icon: const Icon(
-        Icons.menu,
-        semanticLabel: "Menu Button",
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              color: Colors.white,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
       ),
-      onPressed: () {
-        print("Menu clicked");
-      },
-    ),
-    // TODO: Add trailing buttons (102)
-actions: <Widget>[
-  IconButton(
-    icon: const Icon(
-      Icons.search,
-      semanticLabel: 'search',
-    ),
-    onPressed: () {
-      print('Search button');
-    },
-  ),
-  IconButton(
-    icon: const Icon(
-      Icons.tune,
-      semanticLabel: 'filter',
-    ),
-    onPressed: () {
-      print('Filter button');
-    },
-  ),
-],
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context)
       ),
     );
   }
 }
+
