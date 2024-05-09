c988f836a86455167de52bc48b93522dfed16ead
Dwisaputr <121742036+Dwisaputr@users.noreply.github.com>
2024-05-09 19:21:03 +0700

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,88 +1,110 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
+  import 'package:intl/intl.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  import 'model/product.dart';
+  import 'model/products_repository.dart';
+  import 'supplemental/asymmetric_view.dart';
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
+  class HomePage extends StatelessWidget {
+    const HomePage({Key? key}) : super(key: key);
 
-class _LoginPageState extends State<LoginPage> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
+    List<Card> _buildGridCards(BuildContext context) {
+      List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            const SizedBox(height: 80.0),
-            Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
-              ],
-            ),
-            const SizedBox(height: 120.0),
-            // TODO: Remove filled: true values (103)
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                filled: true,
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 12.0),
-            OverflowBar(
-              alignment: MainAxisAlignment.end,
-              children: <Widget>[
-                // TODO: Add a beveled rectangular border to CANCEL (103)
-                TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    _usernameController.clear();
-                    _passwordController.clear();
-                  },
+      if (products.isEmpty) {
+        return const <Card>[];
+      }
+
+      final ThemeData theme = Theme.of(context);
+      final NumberFormat formatter = NumberFormat.simpleCurrency(
+          locale: Localizations.localeOf(context).toString());
+
+      return products.map((product) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          // TODO: Adjust card heights (103)
+          elevation: 0.0,
+          child: Column(
+            // TODO: Center items on the card (103)
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18 / 11,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.fitWidth,
                 ),
-                // TODO: Add an elevation to NEXT (103)
-                // TODO: Add a beveled rectangular border to NEXT (103)
-                ElevatedButton(
-                  child: const Text('NEXT'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    // TODO: Align labels to the bottom and center (103)
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    // TODO: Change innermost Column (103)
+                    children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                      Text(
+                        product.name,
+                        style: theme.textTheme.labelLarge,
+                        softWrap: false,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.bodySmall,
+                      ),
+                      // End new code
+                    ],
+                  ),
                 ),
-              ],
+              ),
+            ],
+          ),
+        );
+      }).toList();
+    }
+
+    // TODO: Add a variable for Category (104)
+    @override
+    Widget build(BuildContext context) {
+      // TODO: Return an AsymmetricView (104)
+      // TODO: Pass Category variable to AsymmetricView (104)
+      return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {},
+          ),
+          title: const Text('SHRINE'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {},
             ),
           ],
         ),
-      ),
+        body: AsymmetricView(
+          products: ProductsRepository.loadProducts(Category.all),
+        ),
+        resizeToAvoidBottomInset: false,
     );
   }
 }
